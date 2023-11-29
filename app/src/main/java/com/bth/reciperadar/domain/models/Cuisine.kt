84ca54874851e7349a846599dec3d9ae87d19c58
Bryan Aafjes <bryan.aafjes@gmail.com>
2023-11-29 10:38:16 +0100

    Added models for data

---
+++ app/src/main/java/com/bth/reciperadar/domain/models/Cuisine.kt
@@ -0,0 +1,7 @@
+package com.bth.reciperadar.domain.models
+
+data class Cuisine (
+    var id: String,
+    var title: String,
+    var description: String,
+)
---
+++ app/src/main/java/com/bth/reciperadar/domain/models/DietaryInfo.kt
@@ -0,0 +1,7 @@
+package com.bth.reciperadar.domain.models
+
+data class DietaryInfo (
+    var id: String,
+    var title: String,
+    var description: String,
+)
