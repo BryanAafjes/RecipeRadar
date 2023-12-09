b1e4af1822c2db2f58fa3df70bb53d2a51584295
Bryan Aafjes <bryan.aafjes@gmail.com>
2023-12-09 14:35:23 +0100

    Added more details to ingredient detail page and improved styling

--- app/src/main/java/com/bth/reciperadar/presentation/screens/recipe/RecipeDetailScreen.kt
+++ app/src/main/java/com/bth/reciperadar/presentation/screens/recipe/RecipeDetailScreen.kt
@@ -1,5 +1,12 @@
 package com.bth.reciperadar.presentation.screens.recipe
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -7,7 +14,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.bth.reciperadar.domain.controllers.RecipeController
+import com.bth.reciperadar.presentation.viewmodels.IngredientViewModel
 import com.bth.reciperadar.presentation.viewmodels.RecipeViewModel
 import com.bth.reciperadar.presentation.viewmodels.toViewModel
 import kotlinx.coroutines.Dispatchers
@@ -27,5 +38,49 @@ fun RecipeDetailScreen(
         }
     }
 
-    recipe?.let { Text(it.title) }
+    recipe?.let {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = it.title,
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            it.ingredients?.let { ingredients ->
+                Text(
+                    text = "Ingredients:",
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Column {
+                    ingredients.forEach { ingredient ->
+                        IngredientItem(ingredient)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun IngredientItem(ingredient: IngredientViewModel) {
+    Column(modifier = Modifier.padding(horizontal = 20.dp)) {
+        Text(
+            text = "Name: ${ingredient.name}",
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Amount: ${ingredient.amount}",
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Divider(modifier = Modifier.padding(vertical = 8.dp))
+    }
 }
