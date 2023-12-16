ced6a329c095e39cfe29d80b68f4b323d5d109e3
Bryan Aafjes <bryan.aafjes@gmail.com>
2023-12-16 19:40:36 +0100

    Removed unused imports and added trailing icon

--- app/src/main/java/com/bth/reciperadar/presentation/screens/mainscreen/MainScreen.kt
+++ app/src/main/java/com/bth/reciperadar/presentation/screens/mainscreen/MainScreen.kt
@@ -1,6 +1,5 @@
 package com.bth.reciperadar.presentation.screens.mainscreen
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -12,11 +11,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -32,13 +30,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.bth.reciperadar.R
 import com.bth.reciperadar.domain.controllers.AuthController
 import com.bth.reciperadar.domain.controllers.ProfileController
 import com.bth.reciperadar.domain.controllers.RecipeController
@@ -90,7 +85,7 @@ fun MainScreen(
             text = "Hi, ${profile?.username ?: ""} \uD83D\uDC4B",
             style = MaterialTheme.typography.headlineLarge,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(top = 50.dp, bottom = 20.dp)
+            modifier = Modifier.padding(top = 40.dp, bottom = 20.dp)
         )
         if (showEmailVerifyNotification) {
             Surface(
@@ -134,7 +129,10 @@ fun MainScreen(
                 searchQuery = it
             },
             label = { Text("Search by recipe name") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            trailingIcon = {
+                Icon(imageVector = Icons.Default.Search, contentDescription = null)
+            },
         )
         Spacer(modifier = Modifier.height(8.dp))
         Button(onClick = {
