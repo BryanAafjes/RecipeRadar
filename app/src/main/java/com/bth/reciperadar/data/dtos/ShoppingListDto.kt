491a0a92a1b6efc0c4da14654909ced985082502
Bryan Aafjes <bryan.aafjes@gmail.com>
2023-12-17 14:38:22 +0100

    Added models for shopping list

---
+++ app/src/main/java/com/bth/reciperadar/data/dtos/ShoppingListDto.kt
@@ -0,0 +1,7 @@
+package com.bth.reciperadar.data.dtos
+
+data class ShoppingListDto(
+    var id: String = "",
+    var userId: String = "",
+    var ingredients: List<IngredientDto> = emptyList(),
+)
---
+++ app/src/main/java/com/bth/reciperadar/domain/models/Inventory.kt
@@ -0,0 +1,24 @@
+package com.bth.reciperadar.domain.models
+
+import com.bth.reciperadar.data.dtos.InventoryDto
+data class Inventory(
+    var id: String = "",
+    var userId: String = "",
+    var ingredients: List<Ingredient> = emptyList(),
+)
+
+fun Inventory.toDto(): InventoryDto {
+    return InventoryDto(
+        id = this.id,
+        userId = this.userId,
+        ingredients = this.ingredients.map { it.toDto() },
+    )
+}
+
+fun InventoryDto.toDomain(): Inventory {
+    return Inventory(
+        id = this.id,
+        userId = this.userId,
+        ingredients = this.ingredients.map { it.toDomain() },
+    )
+}
